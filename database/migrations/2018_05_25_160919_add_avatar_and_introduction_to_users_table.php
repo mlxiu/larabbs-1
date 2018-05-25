8eb05d0137ff15c1970f0c8864760866a55c869a
vini123 <lichking_lin86@qq.com>
2018-05-25 09:19:46 +0000

    用户编辑资料

---
+++ database/migrations/2018_05_25_160919_add_avatar_and_introduction_to_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAvatarAndIntroductionToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->nullable()->after('password')->comment('用户头像');
+            $table->string('introduction')->default('美的事物是永恒的喜悦')->after('avatar');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+            $table->dropColumn('introduction');
+        });
+    }
+}
